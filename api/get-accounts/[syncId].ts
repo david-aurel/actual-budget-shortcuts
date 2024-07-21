cf7232784cbbc28cab1ead83241867e32468cee2
David Durlan <53895961+david-aurel@users.noreply.github.com>
2024-07-21 19:32:58 +0200

    debug: update log message

--- api/get-accounts/[syncId].ts
+++ api/get-accounts/[syncId].ts
@@ -3,7 +3,7 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 import fs from 'fs'
 import path from 'path'
 
-const migrationsPath = path.resolve(__dirname, 'node_modules')
+const migrationsPath = path.resolve(__dirname, '../../node_modules')
 
 export default async function (
   request: VercelRequest,
